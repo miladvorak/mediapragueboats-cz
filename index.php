68abed1459d7167ab1b6b98f64956eb35bba6c0b
miladvorak <dvmilan@gmail.com>
2026-02-23 15:53:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php require_once __DIR__ . "/includes/header.php"; ?>
 <header class="page-head">
           <div>
-            <h1>Fleet</h1>
+            <h1>Fleets</h1>
           </div>
         </header>
 
